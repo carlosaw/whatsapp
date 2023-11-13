2343e9bcf55f9cc2925668272293b05fa7997474
carlosaw <carlosfreevg@gmail.com>
2023-11-13 16:50:55 -0400

    Chat-whatsapp

--- src/components/ChatIntro.js
+++ src/components/ChatIntro.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './ChatIntro.css';
 
-export default () => {
+const chatIntro = () => {
     return (
         <div className="chatIntro">
-            <img src="https://web.whatsapp.com/img/intro-connection-light_c98cc75f2aa905314d74375a975d2cf2.jpg" alt="" />
+            <img src="https://th.bing.com/th/id/OIP.69hi-DvSFkRBA4i5sVhR_gHaHa?pid=ImgDet&rs=1" alt="" />
             <h1>Mantenha seu celular conectado</h1>
             <h2>O WhatsApp conecta ao seu telefone para sincronizar suas mensagens. Para reduzir o uso de dados, conecte seu telefone a uma rede Wi-Fi.</h2>
         </div>
     );
 }
+
+export default chatIntro;
--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -71,7 +71,7 @@ export default ({user, data}) => {
     }
 
     const handleInputKeyUp = (e) => {
-        if(e.keyCode == 13) {
+        if(e.keyCode === 13) {
             handleSendClick();
         }
     }
